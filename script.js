045908287ceac70cfa84a01121523511e86d45c2
tarikjamil <tarikjamill@gmail.com>
2023-12-22 11:22:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -154,7 +154,7 @@ gsap.utils.toArray("[animation=split-fade]").forEach((container) => {
 
 gsap.to(".is--sticky", {
   scrollTrigger: {
-    trigger: ".footprint-container",
+    trigger: ".is--sticky",
     start: "top top", // when the top of the trigger hits the top of the viewport
     end: "bottom top", // end after scrolling the height of the element
     pin: true, // enables pinning
